7311a637e17431ab6e3fdda5f1c9ad75d57a5358
nmpiazza <nmp5860@rit.edu>
2015-03-15 23:41:44 -0400

    Add shebang line and version string

--- leSSH.py
+++ leSSH.py
@@ -1,7 +1,19 @@
+#!/usr/bin/env python
+"""
+@Author: Nicholas Piazza
+@Purpose: Identify the least load server and either return it to the user 
+          or launch an SSH session
+"""
+
 import argparse
+import pxssh
+import getpass
 
 def main():
-    pass
+    with open(filename) as f:
+        servers = f.readlines()
+
+
 
 if __name__ == __main__:
     main()
