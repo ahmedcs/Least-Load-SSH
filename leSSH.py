3bf0d91bf5282ea0e472f6694d6c835a8fc18089
nmpiazza <nmp5860@rit.edu>
2015-03-24 18:23:40 -0400

    Add --short-circuit switch and set up base for format of input and server testing

--- leSSH.py
+++ leSSH.py
@@ -10,10 +10,24 @@ import pxssh
 import getpass
 
 def main():
-    with open(filename) as f:
+    #Create my argument parser
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("file", help="File containing a list of SSH servers.")
+    parser.add_argument("--short-circuit", "-sc", help="If a server has 0 users logged on, stop examination of all other servers and return this server as the successful server.", action='store_true')
+    args = parser.parse_args()
+    
+    #Read lines from a file and then strip the newlines
+    with open(args.file) as f:
         servers = f.readlines()
+        servers = map(lambda s: s.strip(), servers)
+
+    serverCount = 0
+    for server in servers:
+        serverCount += 1
+        print "Testing server %d: %s" % (serverCount, server)
 
 
 
-if __name__ == __main__:
+if __name__ == "__main__":
     main()
